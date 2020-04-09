9e66d97eba9effb07cf68489a360f52586993066
MrgaCRO <pavlinmario@gmail.com>
2020-04-10 01:56:16 +0200

    Made Login page

---
+++ lib/ui/molecule/login_page.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<MyApp> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //appBar: AppBar(
+      //title: Text('Login'),
+      //),
+        body: Padding(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Text(
+                  'LOGIN',
+                  style: TextStyle(
+                      color: Colors.green,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 30),
+                ),
+                Text(
+                  'Login in with Email',
+                  style: TextStyle(fontSize: 20),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Email',
+                    ),
+                  ),
+                ),
+                Text(
+                  'Login with Phone',
+                  style: TextStyle(fontSize: 20),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  child: TextField(
+                    controller: phoneController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Phone',
+                    ),
+                  ),
+                ),
+                Container(
+                    height: 50,
+                    width: 200,
+                    child: RaisedButton(
+                      textColor: Colors.white,
+                      color: Colors.green,
+                      child: Text('Login'),
+                      onPressed: () {
+                        print(emailController.text);
+                        print(phoneController.text);
+                      },
+                    )),
+                Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'Or',
+                      style: TextStyle(fontSize: 20),
+                    )),
+                Container(
+                    height: 50,
+                    width: 200,
+                    child: RaisedButton(
+                      textColor: Colors.white,
+                      color: Colors.green,
+                      child: Text('Create account'),
+                      onPressed: () {
+                        //nešto dodati
+                      },
+                    )),
+              ],
+            )));
+  }
+}
