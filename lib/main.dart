77da34aa6a40e26eadd3c1c9df5285222acf9be3
Egle0111 <egle.zukon@pu.t-com.hr>
2020-04-14 16:52:46 +0200

    uvrsten MaterialApp u build widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:youthetree/ui/molecule/map_widget.dart';
 
 void main() {
-  runApp(YTT());
-}
-
-class YTT extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text("YTT"),
-        ),
-      ),
-    );
-  }
+  runApp(YttMapWidget());
 }
