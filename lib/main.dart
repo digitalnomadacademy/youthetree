7ef83f2c1c8fea7731d34bb26047753842887fef
Tinjomar <61094781+Tinjomar@users.noreply.github.com>
2020-04-22 21:38:49 +0200

    onboarding_page_main_dart_change

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class YTT extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: OnBoardingPage (),
+      home: Container (),
     );
   }
 }
