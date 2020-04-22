4096ed1ad3ff41f52fd7b52559f0eb030ae6196e
Noamagnus <milosivackovic@gmail.com>
2020-04-22 22:41:22 +0200

    First commit after review

--- lib/ui/molecule/resize_widget.dart
+++ lib/ui/molecule/resize_widget.dart
@@ -1,72 +1,69 @@
 import 'package:flutter/material.dart';
 
-
 class ResizeWidget extends StatefulWidget {
+  final Widget topChild;
+  final Widget bottomChild;
+  final double dividerPercentage;
+  ResizeWidget({this.topChild, this.bottomChild, this.dividerPercentage});
+
   @override
   _ResizeWidgetState createState() => _ResizeWidgetState();
 }
 
-double containerHeight = 40;
-
 class _ResizeWidgetState extends State<ResizeWidget> {
+  double _currentPercentage;
+  double _currentPosition;
+  @override
+  void initState() {
+    // TODO: implement initState
+    _currentPercentage = widget.dividerPercentage;
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('neki text'),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Expanded(
-            child: Container(
-              child: Center(
-                  child: Text(
-                    'Red content',
-                    style: TextStyle(
-                      fontSize: 25,
-                    ),
-                  )),
-              decoration: BoxDecoration(color: Colors.red),
+    return GestureDetector(
+      child: GestureDetector(
+        onVerticalDragStart: (DragStartDetails details) {
+          _currentPosition =
+              _currentPercentage * (MediaQuery.of(context).size.height);
+        },
+        onVerticalDragUpdate: (DragUpdateDetails details) {
+          setState(() {
+            if ((details.globalPosition.dy) - _currentPosition <= 30 &&
+                _currentPosition - (details.globalPosition.dy) <= 30) {
+              if (_currentPercentage <= widget.dividerPercentage) {
+                _currentPosition = _currentPosition + details.delta.dy;
+                _currentPercentage =
+                    _currentPosition / (MediaQuery.of(context).size.height);
+              }
+            }
+            print('current% $_currentPercentage');
+            print('divider% ${widget.dividerPercentage}');
+//           print (details.globalPosition);
+//            print('global ${details.globalPosition.dy}');
+//            print('current position $_currentPosition');
+//            print(MediaQuery.of(context).size.height);
+          });
+        },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Expanded(
+                flex: (_currentPercentage * 100).toInt(),
+                child: widget.topChild),
+            Divider(
+              height: 0,
+              thickness: 1,
+              color: Colors.black,
             ),
-          ),
-          Divider(
-            height: 0,
-            thickness: 1,
-            color: Colors.black,
-          ),
-          GestureDetector(
-            onVerticalDragStart: (DragStartDetails details) {},
-            onVerticalDragUpdate: (DragUpdateDetails details) {
-              setState(() {
-                if ((details.delta.dy) < 0) {
-                  if (containerHeight <= 500) {
-                    containerHeight = containerHeight - details.delta.dy;
-                  }
-                }
-                if (details.delta.dy > 0) {
-                  if (containerHeight >= 40) {
-                    containerHeight = containerHeight - details.delta.dy;
-                  }
-                }
-                print('container height $containerHeight');
-                print('delta ${details.delta}');
-                print('local ${details.localPosition}');
-              });
-            },
-            child: Container(
-              height: containerHeight,
-              child: Center(
-                  child: Text(
-                    'Green Content',
-                    style: TextStyle(
-                      fontSize: 25,
-                    ),
-                  )),
-              decoration: BoxDecoration(color: Colors.lightGreen),
-            ),
-          ),
-        ],
+            Expanded(
+              flex: ((1 - _currentPercentage) * 100).toInt(),
+              child: widget.bottomChild,
+            )
+          ],
+        ),
       ),
     );
   }
