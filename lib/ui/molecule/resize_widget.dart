6ec9b9bec9782b3a54522c3023bf74757fa71d8c
Noamagnus <milosivackovic@gmail.com>
2020-04-23 11:50:25 +0200

    comments in resize_widget

--- lib/ui/molecule/resize_widget.dart
+++ lib/ui/molecule/resize_widget.dart
@@ -26,22 +26,33 @@ class _ResizeWidgetState extends State<ResizeWidget> {
     return GestureDetector(
       child: GestureDetector(
         onVerticalDragStart: (DragStartDetails details) {
+          //_curentPosition sam uveo da bi _currentPrecentage preveo u vrednosti duzine
+          //kojima GD moze operirati
           _currentPosition =
               _currentPercentage * (MediaQuery.of(context).size.height);
         },
         onVerticalDragUpdate: (DragUpdateDetails details) {
           setState(() {
+            //na ovaj nacin sam definisao opseg koji reaguje na touch
+            //30 pixela iznad i ispod divider-a
             if ((details.globalPosition.dy) - _currentPosition <= 30 &&
                 _currentPosition - (details.globalPosition.dy) <= 30) {
+
+              //pokusaj ogranicavanja opsega sa donje strane ekrana, problem nastaje jer je _widget.dividerPrecentage
+              //zaokruzen na jednu decimalu i _currentPrecentage nekako padne ispod vrednosti _widget.dividerPrecentage
+              //iako je uslovljeno da ne bi smeo. Verovatno zbog vrednosti details.delta.dy kojom update-ujem vrednost
+              // _currentPosition
               if (_currentPercentage <= widget.dividerPercentage) {
                 _currentPosition = _currentPosition + details.delta.dy;
                 _currentPercentage =
                     _currentPosition / (MediaQuery.of(context).size.height);
               }
             }
-            print('current% $_currentPercentage');
+
+            // ovaj deo code-a mi sluzi da bih shvatio valicine a i video kako se one menjaju
+            print('current% $_currentPercentage'); //
             print('divider% ${widget.dividerPercentage}');
-//           print (details.globalPosition);
+//
 //            print('global ${details.globalPosition.dy}');
 //            print('current position $_currentPosition');
 //            print(MediaQuery.of(context).size.height);
