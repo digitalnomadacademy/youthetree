1f8dbe306368aa14f57de76a04f69cefac8fe0b9
Egle0111 <egle.zukon@pu.t-com.hr>
2020-04-23 17:33:57 +0200

    map widget nakon promjene AVD-a - jos ne pokazuje lokaciju

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,8 +7,8 @@ import GoogleMaps
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-        GMSServices.provideAPIKey("AIzaSyDmdyzFEPPoABQKV_w4pCDJozcky9GRPpY")
-        GMSPlacesClient.provideAPIKey("AIzaSyDmdyzFEPPoABQKV_w4pCDJozcky9GRPpY")
+        GMSServices.provideAPIKey("AIzaSyBEbIhBzGarHxIT7AMEf9unrbICrW6FtD8")
+        GMSPlacesClient.provideAPIKey("AIzaSyBEbIhBzGarHxIT7AMEf9unrbICrW6FtD8")
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
