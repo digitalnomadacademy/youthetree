58fdfcdf36241efcb428e816aa398b72e59aee21
MrgaCRO <pavlinmario@gmail.com>
2020-05-07 19:20:09 +0200

    created login page, routes to be edited

--- lib/ui/molecule/login_page.dart
+++ lib/ui/molecule/login_page.dart
@@ -1,97 +1,97 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: LoginPage(),
-  ));
-}
-
 class LoginPage extends StatefulWidget {
   @override
-  _State createState() => _State();
+  _LoginPageState createState() => _LoginPageState();
 }
 
-class _State extends State<LoginPage> {
-  TextEditingController emailController = TextEditingController();
-  TextEditingController phoneController = TextEditingController();
+class _LoginPageState extends State<LoginPage> {
+  var _emailController = TextEditingController();
+  var _phoneController = TextEditingController();
+
+//Focus Nodes
+  var _phoneFocusNode = FocusNode();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //appBar: AppBar(
-      //title: Text('Login'),
-      //),
-        body: Padding(
-            padding: EdgeInsets.all(10),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Text(
-                  'LOGIN',
-                  style: TextStyle(
-                      color: Colors.green,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 30),
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 5),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Spacer(),
+              Text(
+                'Login in with Email',
+                style: TextStyle(fontSize: 20),
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                child: TextFormField(
+                  controller: _emailController,
+                  textInputAction: TextInputAction.go,
+                  onEditingComplete: () => _phoneFocusNode.requestFocus(),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                  ),
                 ),
-                Text(
-                  'Login in with Email',
-                  style: TextStyle(fontSize: 20),
+              ),
+              Text(
+                'Or login with Phone',
+                style: TextStyle(fontSize: 20),
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                child: TextFormField(
+                  controller: _phoneController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Phone',
+                  ),
                 ),
-                Container(
+              ),
+              Spacer(),
+              Container(
+                  height: 50,
+                  width: 200,
+                  child: RaisedButton(
+                    textColor: Colors.white,
+                    color: Colors.green,
+                    child: Text('Login'),
+                    onPressed: () {
+                      //Navigator.pushNamed(context, RouteName.home );
+                    },
+                  ),
+              ),
+              Container(
+                  alignment: Alignment.center,
                   padding: EdgeInsets.all(10),
-                  child: TextField(
-                    controller: emailController,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Email',
-                    ),
+                  child: Text(
+                    'Or',
+                    style: TextStyle(fontSize: 20),
+                  )),
+              Expanded(
+                child: Container(
+                  height: 50,
+                  width: 200,
+                  child: RaisedButton(
+                    textColor: Colors.white,
+                    color: Colors.green,
+                    child: Text('Create account'),
+                    onPressed: () {
+                      Navigator.pushNamed(context, "create");
+                    },
                   ),
                 ),
-                Text(
-                  'Login with Phone',
-                  style: TextStyle(fontSize: 20),
-                ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
-                  child: TextField(
-                    controller: phoneController,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Phone',
-                    ),
-                  ),
-                ),
-                Container(
-                    height: 50,
-                    width: 200,
-                    child: RaisedButton(
-                      textColor: Colors.white,
-                      color: Colors.green,
-                      child: Text('Login'),
-                      onPressed: () {
-                        print(emailController.text);
-                        print(phoneController.text);
-                      },
-                    )),
-                Container(
-                    alignment: Alignment.center,
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      'Or',
-                      style: TextStyle(fontSize: 20),
-                    )),
-                Container(
-                    height: 50,
-                    width: 200,
-                    child: RaisedButton(
-                      textColor: Colors.white,
-                      color: Colors.green,
-                      child: Text('Create account'),
-                      onPressed: () {
-
-                      },
-                    )),
-              ],
-            )));
+              ),
+              Spacer(flex: 3),
+            ],
+          ),
+        ),
+      );
   }
 }
