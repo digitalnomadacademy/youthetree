d00590d152a093e628743167917406026c5028f8
Egle0111 <egle.zukon@pu.t-com.hr>
2020-05-07 15:54:13 +0200

    kreiran page map_test

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -3,13 +3,16 @@ import 'package:youthetree/ui/page/home_page.dart';
 import 'package:youthetree/ui/page/splash_page.dart';
 import 'package:youthetree/ui/page/SplashPage.dart';
 import 'package:youthetree/ui/page/home_page.dart';
+import 'package:youthetree/ui/page/map_test.dart';
+//import 'package:youthetree/ui/page/SplashPage.dart';
+//import 'package:youthetree/ui/page/home_page.dart';
 
 Map<String, WidgetBuilder> routes = {
   RouteName.home: (context) => HomePage(),
   RouteName.splash: (context) => SplashPage(),
 
 // TEST ROUTE
-  RouteName.testMap: (context) => MapWidget(),
+  RouteName.testMap: (context) => MapTest(),
 };
 
 String initialRoute = RouteName.testMap;
