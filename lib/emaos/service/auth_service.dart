fe61e7fd6d96d483ca485a249faba953dd1df06b
matej <matejthetree@gmail.com>
2020-05-11 20:45:46 +0200

    login / logout working user observable hooked up

--- lib/emaos/service/auth_service.dart
+++ lib/emaos/service/auth_service.dart
@@ -4,12 +4,12 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:rxdart/rxdart.dart';
 
 class AuthService {
-  FirebaseAuth _auth = FirebaseAuth.instance;
+  FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
   BehaviorSubject<FirebaseUser> firebaseUser$ = BehaviorSubject();
 
   AuthService() {
-    _auth.onAuthStateChanged.listen((FirebaseUser firebaseUser) {
+    _firebaseAuth.onAuthStateChanged.listen((FirebaseUser firebaseUser) {
       print("authStateChanged $firebaseUser");
       firebaseUser$.add(firebaseUser);
     });
@@ -17,11 +17,11 @@ class AuthService {
 
 //  LOGIN
   Future<void> loginEmail(String email, String password) {
-    return _auth.signInWithEmailAndPassword(email: email, password: password);
+    return _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
   }
 
   Future<void> loginPhone(String phone) {
-    return _auth.verifyPhoneNumber(
+    return _firebaseAuth.verifyPhoneNumber(
         phoneNumber: phone,
         timeout: Duration(seconds: 120),
         verificationCompleted: ((_) => print("verification complete for $_")),
@@ -44,7 +44,7 @@ class AuthService {
     print("AuthService");
     print("create account called with $email $password");
     try {
-      AuthResult authResult = await _auth.createUserWithEmailAndPassword(
+      AuthResult authResult = await _firebaseAuth.createUserWithEmailAndPassword(
           email: email, password: password);
       return authResult.user;
     } catch (e) {
@@ -57,6 +57,6 @@ class AuthService {
 //  LOGOUT
 
   Future<void> logout() async {
-    _auth.signOut();
+    _firebaseAuth.signOut();
   }
 }
